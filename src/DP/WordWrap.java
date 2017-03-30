ff66df8791824dc3d6b10f194bd7330e01f95c41
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2017-03-30 15:17:16 +0530

    DP practice - Word Wrap

--- src/DP/WordWrap.java
+++ src/DP/WordWrap.java
@@ -11,6 +11,7 @@ public class WordWrap {
     static int a[];
     static int limit;
     static int wraps[][];
+    static double cache[][];
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -19,8 +20,13 @@ public class WordWrap {
             a[i]=scan.nextInt();
 
         }
-        wraps = new int[n+1][n+1];
+        wraps = new int[n][n];
+        cache = new double[n][n];
         for (int part[]: wraps
+             ) {
+            Arrays.fill(part,-1);
+        }
+        for (double part[]: cache
              ) {
             Arrays.fill(part,-1);
         }
@@ -44,32 +50,36 @@ public class WordWrap {
     }
 
     private static double wordWrap(int i,int j) {
-        if(i==j){
-            return Math.pow(limit-a[i],2);
-        }
-        if(j==i+1){
-            if(a[i]+a[j]+1<=limit)
-                return Math.pow(limit-a[i]-a[j]-1,2);
-            else
-                return Math.pow(limit-a[i],2)+Math.pow(limit-a[j],2);
+        if(cache[i][j]!=-1)
+            return cache[i][j];
 
+        else if(i==j){
+            cache[i][j]=Math.pow(limit-a[i],2);
         }
-        int sum=0;
-        double min = Double.MAX_VALUE;
-        for(int k=i;k<j;k++){
-            if(k==i)
-                sum+=a[k];
+        else if(j==i+1){
+            if(a[i]+a[j]+1<=limit)
+                cache[i][j]= Math.pow(limit-a[i]-a[j]-1,2);
             else
-                sum+=a[k]+1;
-            if(sum<=limit){
-                double res = Math.pow((limit-sum),2)+wordWrap(k+1,j);
-                if(res<min) {
-                    min = res;
-                    wraps[i][j]=k;
-                }
+                cache[i][j]= Math.pow(limit-a[i],2)+Math.pow(limit-a[j],2);
+
+        }else {
+            int sum = 0;
+            double min = Double.MAX_VALUE;
+            for (int k = i; k < j; k++) {
+                if (k == i)
+                    sum += a[k];
+                else
+                    sum += a[k] + 1;
+                if (sum <= limit) {
+                    double res = Math.pow((limit - sum), 2) + wordWrap(k + 1, j);
+                    if (res < min) {
+                        min = res;
+                        wraps[i][j] = k;
+                    }
+                } else break;
             }
-            else break;
+            cache[i][j]= min;
         }
-        return min;
+        return cache[i][j];
     }
 }
